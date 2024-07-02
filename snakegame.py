7943e15413608ff671974580b45be1eea96512c1
talindev <talin.dev1@gmail.com>
2024-07-02 14:54:29 -0300

    Add files via upload
    + Comments

--- snakegame.py
+++ snakegame.py
@@ -1,17 +1,19 @@
+# importar as bibliotecas
 import pygame
 import random
 
+# inicializar pygame
 pygame.init()
 
-FOOD_X = random.randint(1,850)
-FOOD_Y = random.randint(1,850)
-SQUARE_X = random.randint(1,850)
-SQUARE_Y = random.randint(1,850)
+# declações dos limites de espaço
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
+FOOD_X = random.randint(1,SCREEN_WIDTH - 50)
+FOOD_Y = random.randint(1,SCREEN_HEIGHT - 50)
+SQUARE_X = random.randint(1,SCREEN_WIDTH - 50)
+SQUARE_Y = random.randint(1,SCREEN_HEIGHT - 50)
 
-# FONTES
-
+# fontes e textos
 font = pygame.font.Font('freesansbold.ttf', 32)
 
 none = font.render('', True, (40,40,40))
@@ -24,53 +26,61 @@ congrats_square = congrats.get_rect()
 congrats_square.centerx = SCREEN_WIDTH // 2
 congrats_square.top = 10
 
+# sistema de score
 score_value = 0
 score = font.render(str(score_value), True, (255,255,255),)
 score_square = score.get_rect()
 
+# para checagem do spawn do texto de parabéns aos 10 pontos
 congrats_spawn = False
 
-# CHECAGEM DA COMIDA
-
+# checagem de spawn da comida
 food_spawn = True
 
-
+# aleatorização da posição
 def random_pos():
     return random.randint(1,1000), random.randint(1,1000)
 
+# declaração da janela do jogo
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+# estrutura retangular dos 2 elementos principais (cobra e comida)
 square = pygame.Rect((SQUARE_X,SQUARE_Y,50,50))
 food = pygame.Rect((FOOD_X,FOOD_Y,50,50))
 
+# array de segmentos para incremento do corpo da cobra
 segments = [square.copy()]
 length = 1
 
+
+# ÁREA DE TESTE OU RASCUNHO
 # square_center = square.copy()
 # food_center = food.copy()
 
+# declaração de cor 'color'
 color = (0,255,0)
 
+# loop do jogo
 run = True
 while run:
 
+    # preenchimento de tela para não bugar os segmentos
     screen.fill((40,40,40))
 
+    # inserção das fontes e textos
     none_square = none.get_rect(centerx=SCREEN_WIDTH // 2, top=10)
     congrats_square = congrats.get_rect(centerx=SCREEN_WIDTH // 2, top=10)
 
+    # inserção do sistema de score
     score = font.render('score: ' + str(score_value), True, (255, 255, 255), )
     screen.blit(score, score_square)
 
+    # desenho da cobra
     for segment in segments:
         pygame.draw.rect(screen, color, segment)
     pygame.draw.rect(screen, (255, 0, 0), food)
 
-    if segments[0].colliderect(food) == 1:
-        color = (0,0,255)
-    else:
-        color = (0,255,0)
-
+    # sistema de pressionamento de teclas e movimentação do jogador
     key = pygame.key.get_pressed()
     if key[pygame.K_w] == True:
         square.move_ip(0, -1)
@@ -81,27 +91,34 @@ while run:
     elif key[pygame.K_d] == True:
         square.move_ip(1, 0)
 
-    segments.append(square.copy())
-    segments = segments[-length:]
+    segments.append(square.copy()) # adição de quadrados novos no array por meio do comando 'append'
 
-    if segments[0].colliderect(food):
+
+    segments = segments[-length:] # Em Python, o operador de slicing [:] permite obter uma sublista de uma lista.
+                                  # segments[-length:] usa índices negativos. Em Python, um índice negativo começa do final da lista,
+                                  # com -1 sendo o último elemento, -2 sendo o penúltimo, e assim por diante.
+                                  # segments[-length:] significa "pegue os últimos 'length' elementos da lista 'segments'".
+                                  # Isso garante que, independentemente do número de segmentos adicionados,
+                                  # apenas os 'length' segmentos mais recentes sejam mantidos na lista.
+
+    if segments[0].colliderect(food): # checagem de colisão para incrementar a cobra, aumentando o score e adicionando os valores para o array
         FOOD_X, FOOD_Y = random_pos()
         food.topleft = (FOOD_X, FOOD_Y)
         score_value += 1
         length += 50
 
-    if score_value == 10:
+    if score_value == 10: # ativação da mensagem de parabéns
         screen.blit(congrats, congrats_square)
         congrats_spawn = True
 
-    if congrats_spawn == True:
+    if congrats_spawn == True: # desativação da mensagem de parabéns
         screen.blit(none, none_square)
         congrats_spawn = False
 
-    for event in pygame.event.get():
+    for event in pygame.event.get(): # evento de fechar o jogo
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
+    pygame.display.update() # atualização dos conteúdos do jogo
 
 pygame.quit()
